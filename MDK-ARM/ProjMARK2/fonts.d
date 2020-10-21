7298c8512e36fb42c6a0c1e8ec1032dc0eb47ce4
9ine2543 <9.nine2543@gmail.com>
2020-10-21 18:59:29 +0700

    init commit

---
+++ MDK-ARM/ProjMARK2/fonts.d
@@ -0,0 +1,3 @@
+projmark2\fonts.o: ..\ili9341\fonts.c
+projmark2\fonts.o: ..\ili9341\fonts.h
+projmark2\fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ Src/main.c
@@ -0,0 +1,356 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdarg.h>
+#include "ili9341.h"
+#include "fonts.h"
+#include "testimg.h"
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint16_t xx = 1;
+uint16_t yy = 81;
+uint32_t adc_val[800];
+char coordinateText[20];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void UART_Printf(const char* fmt, ...) {
+    char buff[256];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buff, sizeof(buff), fmt, args);
+    HAL_UART_Transmit(&huart2, (uint8_t*)buff, strlen(buff),
+                      HAL_MAX_DELAY);
+    va_end(args);
+}
+
+void init() {
+    ILI9341_Unselect();
+    ILI9341_Init();
+}
+void start(){
+		ILI9341_FillScreen(ILI9341_BLACK);
+		for(int x = 0; x < ILI9341_WIDTH; x++) {
+					ILI9341_DrawPixel(x, 80, ILI9341_WHITE);
+					ILI9341_DrawPixel(x, ILI9341_HEIGHT-1, ILI9341_WHITE);
+			}
+		for(int y = 80; y < ILI9341_HEIGHT; y++) {
+				ILI9341_DrawPixel(0, y, ILI9341_WHITE);
+				ILI9341_DrawPixel(ILI9341_WIDTH-1, y, ILI9341_WHITE);
+		}
+}
+void loop() {
+    if(HAL_SPI_DeInit(&hspi1) != HAL_OK) {
+        UART_Printf("HAL_SPI_DeInit failed!\r\n");
+        return;
+    }
+
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+
+    if(HAL_SPI_Init(&hspi1) != HAL_OK) {
+        UART_Printf("HAL_SPI_Init failed!\r\n");
+        return;
+    }
+
+    // Check border
+    ILI9341_FillScreen(ILI9341_BLACK);
+
+    for(int x = 0; x < ILI9341_WIDTH; x++) {
+        ILI9341_DrawPixel(x, 0, ILI9341_RED);
+        ILI9341_DrawPixel(x, ILI9341_HEIGHT-1, ILI9341_RED);
+    }
+
+    for(int y = 0; y < ILI9341_HEIGHT; y++) {
+        ILI9341_DrawPixel(0, y, ILI9341_RED);
+        ILI9341_DrawPixel(ILI9341_WIDTH-1, y, ILI9341_RED);
+    }
+
+    HAL_Delay(3000);
+
+    // Check fonts
+    ILI9341_FillScreen(ILI9341_BLACK);
+    ILI9341_WriteString(0, 0, "Font_7x10, red on black, lorem ipsum dolor sit amet", Font_7x10, ILI9341_RED, ILI9341_BLACK);
+    ILI9341_WriteString(0, 3*10, "Font_11x18, green, lorem ipsum dolor sit amet", Font_11x18, ILI9341_GREEN, ILI9341_BLACK);
+    ILI9341_WriteString(0, 3*10+3*18, "Font_16x26, blue, lorem ipsum dolor sit amet", Font_16x26, ILI9341_BLUE, ILI9341_BLACK);
+
+    HAL_Delay(1000);
+    ILI9341_InvertColors(true);
+    HAL_Delay(1000);
+    ILI9341_InvertColors(false);
+
+    HAL_Delay(5000);
+
+    // Check colors
+    ILI9341_FillScreen(ILI9341_WHITE);
+    ILI9341_WriteString(0, 0, "WHITE", Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_BLUE);
+    ILI9341_WriteString(0, 0, "BLUE", Font_11x18, ILI9341_BLACK, ILI9341_BLUE);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_RED);
+    ILI9341_WriteString(0, 0, "RED", Font_11x18, ILI9341_BLACK, ILI9341_RED);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_GREEN);
+    ILI9341_WriteString(0, 0, "GREEN", Font_11x18, ILI9341_BLACK, ILI9341_GREEN);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_CYAN);
+    ILI9341_WriteString(0, 0, "CYAN", Font_11x18, ILI9341_BLACK, ILI9341_CYAN);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_MAGENTA);
+    ILI9341_WriteString(0, 0, "MAGENTA", Font_11x18, ILI9341_BLACK, ILI9341_MAGENTA);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_YELLOW);
+    ILI9341_WriteString(0, 0, "YELLOW", Font_11x18, ILI9341_BLACK, ILI9341_YELLOW);
+    HAL_Delay(500);
+
+    ILI9341_FillScreen(ILI9341_BLACK);
+    ILI9341_WriteString(0, 0, "BLACK", Font_11x18, ILI9341_WHITE, ILI9341_BLACK);
+    HAL_Delay(500);
+
+    ILI9341_DrawImage((ILI9341_WIDTH - 240) / 2, (ILI9341_HEIGHT - 240) / 2, 240, 240, (const uint16_t*)test_img_240x240);
+
+    HAL_Delay(1000);
+
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
+  MX_USART2_UART_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	
+	
+	HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
+	HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
+	int val = 0, val2;
+	int beforeSize = 0, status;
+	char penSize[20], color[20];
+	init();
+	start();
+	HAL_ADC_Start_DMA(&hadc1, adc_val, 800); 
+  while (1)
+  {
+		beforeSize = val;
+		val = TIM2->CNT>>2;
+		val2 = TIM3->CNT>>2;
+		if(beforeSize == val)
+			status = 1;
+		else
+			status = 0;
+		sprintf(penSize, "penSize : %02d", val);
+		if(val2 == 0){
+			sprintf(color, "color : Black");
+		}else if(val2 == 1 || val2 == 2){
+			sprintf(color, "color :   Red");
+		}else if(val2 == 3 || val2 == 4){
+			sprintf(color, "color : White");
+		}
+		ILI9341_WriteString(10, 30, penSize, Font_11x18, ILI9341_RED, ILI9341_BLACK);
+		ILI9341_WriteString(10, 50, color, Font_11x18, ILI9341_RED, ILI9341_BLACK);
+		sprintf(coordinateText, "x : %03d y : %03d", xx, yy);
+		ILI9341_WriteString(10, 10, coordinateText, Font_11x18, ILI9341_RED, ILI9341_BLACK);
+		
+		if(adc_val[0] > 2200){
+			if(xx < 238 - val * 2)
+				xx +=2;
+		}else if(adc_val[0] < 1900){
+			if(xx > 1)
+				xx-=2;
+		}
+		if(adc_val[1] > 2200){
+			if(yy < 318 - val * 2)
+				yy+=2;
+		}else if(adc_val[1] < 1900){
+			if(yy > 81)
+				yy-=2;
+		}
+		//HAL_Delay(50);
+		if(status == 0){
+			ILI9341_FillRectangle(xx, yy, beforeSize*2, beforeSize*2, ILI9341_BLACK);
+		}
+		ILI9341_FillRectangle(xx, yy, val*2, val*2, ILI9341_BLUE);
+		
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hdma_adc1){
+	
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
